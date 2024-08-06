ca93ecbb25731f0026e8a79b511020cfa8c1d098
pankajparihar0 <128948229+pankajparihar0@users.noreply.github.com>
2024-08-06 18:27:46 +0530

    updated

--- team-portfolio/script.js
+++ team-portfolio/script.js
@@ -2,9 +2,12 @@
 let di=function (){
     document.getElementById('box').style.zIndex=100;
     document.getElementById('box').style.display="block";
+
 };
-let close=function(){
-   
+let cls=function(){
+    document.getElementById('box').style.display='none';
+    document.getElementById('box').style.zIndex=-1;
+    document.getElementById('portfolio').src="";
 }
 document.getElementById("close").addEventListener('click',()=>{
     console.log("gcx");
